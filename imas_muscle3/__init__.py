8c1ac7385f805a022e31f74e31849f19d4b050f7
Olivier Hoenen <Olivier.Hoenen@iter.org>
2025-06-18 21:39:24 +0200

    replace versioneer by setuptools_scm

--- imas_muscle3/__init__.py
+++ imas_muscle3/__init__.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 
-from . import _version
-
-__version__ = _version.get_versions()["version"]
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
 
 version = __version__
-
 
 def get_project_root() -> Path:
     return Path(__file__).resolve().parent.parent
