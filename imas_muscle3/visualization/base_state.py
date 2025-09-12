36595febf3d8f4181c324d996b40761df198d3a4
Sebbe Blokhuizen <sblokhuizen@ignitioncomputing.com>
2025-09-12 14:43:28 +0200

    cleanup

--- imas_muscle3/visualization/base_state.py
+++ imas_muscle3/visualization/base_state.py
@@ -5,16 +5,18 @@ import xarray as xr
 
 
 class BaseState(param.Parameterized):
-    data = param.ClassSelector(class_=xr.Dataset, default=xr.Dataset())
+    data = param.ClassSelector(class_=xr.Dataset)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self._initialize_data()
-
-    @abc.abstractmethod
-    def _initialize_data(self):
-        pass
 
     @abc.abstractmethod
     def update(self, ids):
         pass
+
+
+class BasePlotter(param.Parameterized):
+    state = param.Parameter()
+
+    def __init__(self, state, **params):
+        super().__init__(state=state, **params)
--- imas_muscle3/visualization/states.py
+++ imas_muscle3/visualization/states.py
@@ -4,30 +4,10 @@ import pandas as pd
 import param
 import xarray as xr
 
-# Import the base class from our library file
-from imas_muscle3.visualization.base_state import BaseState
+from imas_muscle3.visualization.base_state import BasePlotter, BaseState
 
 
 class EquilibriumState(BaseState):
-    def _initialize_data(self):
-        self.data = xr.Dataset(
-            data_vars={
-                "ip": ("time", np.array([], dtype=float)),
-                "f_df_dpsi": ("time", np.array([], dtype=float)),
-                "psi": ("time", np.array([], dtype=float)),
-                "profiles_2d": (
-                    ("time", "profile_dim1", "profile_dim2"),
-                    np.empty((0, 0, 0), dtype=float),
-                ),
-            },
-            coords={
-                "time": np.array([], dtype=float),
-                "profile": np.array([], dtype=int),
-                "profile_dim1": np.array([], dtype=int),
-                "profile_dim2": np.array([], dtype=int),
-            },
-        )
-
     def update(self, ids):
         if not ids:
             return
@@ -52,24 +32,13 @@ class EquilibriumState(BaseState):
             },
         )
 
-        if self.data.sizes["time"] == 0:
+        if not self.data:
             self.data = new_point
         else:
             self.data = xr.concat([self.data, new_point], dim="time", join="outer")
 
 
 class PfActiveState(BaseState):
-    def _initialize_data(self):
-        self.data = xr.Dataset(
-            data_vars={
-                "currents": (("time", "coil"), np.empty((0, 0), dtype=float)),
-            },
-            coords={
-                "time": np.array([], dtype=float),
-                "coil": np.array([], dtype=str),
-            },
-        )
-
     def update(self, ids):
         if not ids:
             return
@@ -88,78 +57,63 @@ class PfActiveState(BaseState):
             },
         )
 
-        if self.data.sizes.get("time", 0) == 0:
+        if not self.data:
             self.data = new_point
         else:
             self.data = xr.concat([self.data, new_point], dim="time", join="outer")
 
 
-class Plots(param.Parameterized):
-    """A container for plotting methods that depend on state data."""
-
-    state = param.Parameter()
-
-    def __init__(self, state, **params):
-        super().__init__(state=state, **params)
-
+class Plots(BasePlotter):
     @param.depends("state.data")
     def plot_ip_vs_time(self):
-        xlabel = "Time (s)"
-        ylabel = "Ip (A)"
+        xlabel = "Time [s]"
+        ylabel = "Ip [A]"
 
-        if not self.state or self.state.data.time.size == 0:
-            return hv.Curve(([], []), xlabel, ylabel).opts(
-                framewise=True,
-                height=300,
-                width=960,
-                title="Waiting for data...",
+        if self.state.data:
+            time = self.state.data.time
+            ip = self.state.data.ip
+            title = (
+                f"Ip over time, current t={self.state.data.time[-1].item():.3f} "
+                f"len={len(self.state.data.time)}"
             )
+        else:
+            time = []
+            ip = []
+            title = "Waiting for data..."
 
-        return hv.Curve(
-            (self.state.data.time, self.state.data.ip), xlabel, ylabel
-        ).opts(
-            framewise=True,
-            height=300,
-            width=960,
-            title=f"Ip over time, current t={self.state.data.time[-1].item():.3f}, len={len(self.state.data.time)}",
+        return hv.Curve((time, ip), xlabel, ylabel).opts(
+            framewise=True, height=300, width=960, title=title
         )
 
     @param.depends("state.data")
-    def plot_profile(self):
+    def plot_f_df_dpsi_profile(self):
         xlabel = "Psi"
         ylabel = "ff'"
 
-        if not self.state or self.state.data.time.size == 0:
-            return hv.Curve(([], []), xlabel, ylabel).opts(
-                framewise=True,
-                height=300,
-                width=960,
-                title="Waiting for data...",
-            )
-        latest_data = self.state.data.isel(time=-1)
-        return hv.Curve((latest_data.psi, latest_data.f_df_dpsi), xlabel, ylabel).opts(
-            framewise=True,
-            height=300,
-            width=960,
-            title=f"ff' profile at t={latest_data.time.item():.3f}",
+        if self.state.data:
+            latest_data = self.state.data.isel(time=-1)
+            psi = latest_data.psi
+            f_df_dpsi = latest_data.f_df_dpsi
+            title = f"ff' profile at t={latest_data.time.item():.3f}"
+        else:
+            psi = []
+            f_df_dpsi = []
+            title = "Waiting for data..."
+        return hv.Curve((psi, f_df_dpsi), xlabel, ylabel).opts(
+            framewise=True, height=300, width=960, title=title
         )
 
     # TODO: this plot sometimes doesn't update properly
     @param.depends("state.data")
     def plot_profile_waterfall(self):
-        if not self.state or self.state.data.time.size == 0:
-            return hv.HeatMap(pd.DataFrame(columns=["Time", "Psi Index", "ff'"])).opts(
-                cmap="viridis",
-                colorbar=True,
-                framewise=True,
-                height=300,
-                width=960,
-                title="ff' over time",
-            )
-
-        times = self.state.data.time.values
-        profiles = self.state.data.profile.values
-        f_values_2d = self.state.data.f_df_dpsi.values
+        if self.state.data:
+            times = self.state.data.time.values
+            profiles = self.state.data.profile.values
+            f_values_2d = self.state.data.f_df_dpsi.values
+        else:
+            times = []
+            profiles = []
+            f_values_2d = []
 
         df = pd.DataFrame(
             {
@@ -180,53 +134,45 @@ class Plots(param.Parameterized):
 
     @param.depends("state.data")
     def plot_2d_profile(self):
-        if not self.state or self.state.data.time.size == 0:
-            return hv.Image([]).opts(
-                cmap="viridis",
-                colorbar=True,
-                framewise=True,
-                height=300,
-                width=960,
-                title="Waiting for data...",
-            )
+        if self.state.data:
+            latest_data = self.state.data.isel(time=-1)
+            f_2d = latest_data.profiles_2d.values
+            title = f"Poloidal flux at t={latest_data.time.item():.3f}"
+        else:
+            f_2d = []
+            title = "Waiting for data..."
 
-        latest_data = self.state.data.isel(time=-1)
-
-        f_2d = latest_data.profiles_2d.values
         return hv.Image(f_2d).opts(
             cmap="viridis",
             colorbar=True,
             framewise=True,
             height=300,
             width=960,
-            title=f"Poloidal flux at t={latest_data.time.item():.3f}",
+            title=title,
         )
 
     @param.depends("state.data")
     def plot_coil_currents(self):
-        xlabel = "Time (s)"
-        ylabel = "Coil currents (A)"
+        xlabel = "Time [s]"
+        ylabel = "Coil currents [A]"
 
-        if not self.state or self.state.data.time.size == 0:
-            return hv.Curve(([], []), xlabel, ylabel).opts(
-                framewise=True,
-                title="Waiting for data...",
-            )
-
-        curves = [
-            hv.Curve(
-                (self.state.data.time, self.state.data.currents.sel(coil=coil)),
-                xlabel,
-                ylabel,
-                label=str(coil),
-            ).opts(
-                framewise=True,
-                height=500,
-                width=960,
-                title=f"coil currents over time, current t={self.state.data.time[-1].item():.3f}",
-            )
-            for coil in self.state.data.coil.values
-        ]
+        if self.state.data:
+            curves = [
+                hv.Curve(
+                    (self.state.data.time, self.state.data.currents.sel(coil=coil)),
+                    xlabel,
+                    ylabel,
+                    label=str(coil),
+                ).opts(
+                    framewise=True,
+                    height=500,
+                    width=960,
+                    title=f"coil currents over time, current t={self.state.data.time[-1].item():.3f}",
+                )
+                for coil in self.state.data.coil.values
+            ]
+        else:
+            curves = [hv.Curve(([0, 1, 2], [0, 1, 2]), xlabel, ylabel)]
 
         return hv.Overlay(curves)
 
@@ -239,26 +185,26 @@ DASHBOARD_LAYOUT = [
     {
         "plot_class": Plots,
         "state_name": "equilibrium",
-        "plot_method": "plot_profile",
+        "plot_method": "plot_f_df_dpsi_profile",
     },
     {
         "plot_class": Plots,
         "state_name": "equilibrium",
         "plot_method": "plot_ip_vs_time",
     },
-    {
-        "plot_class": Plots,
-        "state_name": "equilibrium",
-        "plot_method": "plot_profile_waterfall",
-    },
+    # {
+    #     "plot_class": Plots,
+    #     "state_name": "equilibrium",
+    #     "plot_method": "plot_profile_waterfall",
+    # },
     {
         "plot_class": Plots,
         "state_name": "equilibrium",
         "plot_method": "plot_2d_profile",
     },
-    {
-        "plot_class": Plots,
-        "state_name": "pf_active",
-        "plot_method": "plot_coil_currents",
-    },
+    # {
+    #     "plot_class": Plots,
+    #     "state_name": "pf_active",
+    #     "plot_method": "plot_coil_currents",
+    # },
 ]
